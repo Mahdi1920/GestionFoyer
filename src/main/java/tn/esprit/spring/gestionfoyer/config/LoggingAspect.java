48235c97b7acd39784ce358c1ecdc9e3e2ceb809
mahdi <mahdichabbouh98@gmail.com>
2024-11-22 21:37:15 +0100

    aop

--- src/main/java/tn/esprit/spring/gestionfoyer/config/LoggingAspect.java
+++ src/main/java/tn/esprit/spring/gestionfoyer/config/LoggingAspect.java
@@ -2,26 +2,34 @@ package tn.esprit.spring.gestionfoyer.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 @Slf4j
 public class LoggingAspect {
+    // before execution
     @Before("execution(* tn.esprit.spring.gestionfoyer..*(..))")
     public void logMethodEntry(JoinPoint joinPoint) {
         String name = joinPoint.getSignature().getName();
         log.info("In method " + name + " : ");
     }
+
+    // After Execution of methode
+    @After("execution(private String tn.esprit.spring.gestionfoyer..*(..))")
+    public void logMethodEntryAfter(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.info("In method " + name + " : ");
+    }
+
+    // after good execution of methode
     @AfterReturning("execution(* tn.esprit.spring.gestionfoyer..add*(..))")
-    public void logMethodEntryReturn(JoinPoint joinPoint) {
+    public void logMethodEntryAfterReturn(JoinPoint joinPoint) {
         String name = joinPoint.getSignature().getName();
         log.info("Succeffuly adding In method " + name + " : ");
     }
+    //after throw exception
     @AfterThrowing("execution(* tn.esprit.spring.gestionfoyer.servicesImpl.retrieve*(..))")
     public void logMethodEntryThrow(JoinPoint joinPoint) {
         String name = joinPoint.getSignature().getName();
